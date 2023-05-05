8537f71530f1ca44dfec827072cd796f59e91872
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-05-05 18:56:23 +0530

    second commit

--- web1.py
+++ web1.py
@@ -3,4 +3,14 @@ from bs4 import BeautifulSoup
 with open("basic.html",'r') as f:
     output=BeautifulSoup(f,"html.parser")
 
-print(output)
+#print(output.prettify())
+
+# tag=output.title
+# print(tag.string)
+# tag.string="abhishek"
+# print(tag)
+# print(output.prettify())
+
+tags=output.find_all("a")
+for i in tags:
+    print(i.text)

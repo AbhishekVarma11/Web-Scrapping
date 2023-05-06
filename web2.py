09296c348a010ea6e14057c9fbe4307e3df4bfde
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-05-06 16:54:29 +0530

    explore some methods

--- web2.py
+++ web2.py
@@ -7,3 +7,9 @@ print(out.prettify())
 tags=out.find_all('h5')
 for txt in tags:
     print(txt.text)
+cards=out.find_all('div',class_='card')
+for card in cards:
+    course=card.h5.text
+    price=card.a.text.split()[-1]
+    print(f"course:{course}{'  '}price:{price}")
+    
